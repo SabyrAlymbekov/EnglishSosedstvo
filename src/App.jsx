c716a8283920f77bae637d8cee2a8c2eb6f5aa61
Alymbekov-Sabyr <alymbekovsabyr@gmail.com>
2024-06-01 15:57:32 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './css/App.css'
+import Header from './header.jsx'
+import Main from './main.jsx'
+
+function App() {
+  return (
+    <>
+        <Header></Header>
+        <Main></Main>
+    </>
+  )
+}
+
+export default App
---
+++ src/nmain.jsx
@@ -0,0 +1,14 @@
+import '/css/main.css'
+
+export default function Main() {
+    return (
+        <main className="main">
+            <div className="blur"></div>
+            <div className="container">
+                <div className="main__content">
+                    <h1 className="main__text">JOIN LOREM IPSUM DOLOR</h1>
+                </div>
+            </div>
+        </main>
+    )
+}
