11b79cfe1bb4c05cbefeb3f012812d58944e875f
Bekjan777 <47937052+Bekjan777@users.noreply.github.com>
2024-06-01 17:21:44 +0600

    main

--- src/nmain.jsx
+++ src/nmain.jsx
@@ -6,7 +6,16 @@ export default function Main() {
             <div className="blur"></div>
             <div className="container">
                 <div className="main__content">
-                    <h1 className="main__text">JOIN LOREM IPSUM DOLOR</h1>
+                    <p className="main__upper">HELPING AGENCIES STAND OUT IN THE RED OCEAN MARKET WE ARE IN WITH A.I</p>
+                    <div className="main__div__of__text">
+                        <h1 className="main__text">
+                            JOIN THE 3,000+ AGENCIES AND
+                            <br/>
+                            CONSULTANTS SELLING THEIR SERVICES
+                            <br/>
+                            AS "AI GROWTH INFRASTRUCTURES"</h1>
+                    </div>
+                    <a href="#" className="button">Click me bro, please</a>
                 </div>
             </div>
         </main>
